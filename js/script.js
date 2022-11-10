5825dfb8c0b401064dae9243abf4208856e9b286
Hizir OZCELIK <ozcelikh@sheridancollege.ca>
2022-11-10 00:22:26 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Hizir Ozcelik | Student ID: 991578495
+//file name: main.js | This is main JS file for Assignment-1
+//Date: June 5th, 2022
+
+// main entry point
+document.addEventListener("DOMContentLoaded", main);  
+
+function main()
+{
+    let row=15;
+    let column=15;
+    let numberOfWord=15;
+    let levels=['Grade-1','Grade-2','Grade-3','Grade-4','Grade-5'];
+    
+
+
+        // event handlers
+
+}
